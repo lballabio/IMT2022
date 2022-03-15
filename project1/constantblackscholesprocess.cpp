fdcb608121b43535f67dbf8336c0fc5fcbf35cb2
RoCl-24 <94844914+RoCl-24@users.noreply.github.com>
2022-03-15 17:51:32 +0100

    Update constantblackscholesprocess.cpp

--- project1/constantblackscholesprocess.cpp
+++ project1/constantblackscholesprocess.cpp
@@ -1,3 +1,45 @@
-
 #include "constantblackscholesprocess.hpp"
 
+constantblackscholesprocess::constantblackscholesprocess() {
+    process = new constantblackscholesprocess();
+    process.spot = 100
+    process.rf_rate = 0.05;
+    process.dividend = 3;
+    process.volatility = 1;
+}
+
+constantblackscholesprocess::constantblackscholesprocess(constantblackscholesprocess process_ref) {
+    process = new constantblackscholesprocess();
+    process.spot = process_ref.getSpot();
+    process.rf_rate = process_ref.getRf_rate();
+    process.dividend = process_ref.getDividend();
+    process.volatility = process_ref.getVolatility();
+}
+
+constantblackscholesprocess::constantblackscholesprocess(double spot1, double rf_ratee2, double dividend1, double volatility1) {
+    process = new constantblackscholesprocess();
+    process.spot = spot1;
+    process.rf_rate = rf_rate1;
+    process.dividend = dividend1;
+    process.volatility = volatility1;
+}
+
+constantblackscholesprocess::~constantblackscholesprocess() {
+
+}
+
+constantblackscholesprocess::getSpot() {
+    return spot;
+}
+
+constantblackscholesprocess::getRf_rate() {
+    return rf_rate;
+}
+
+constantblackscholesprocess::getDividend() {
+    return dividend;
+}
+
+constantblackscholesprocess::getVolatility() {
+    return volatility;
+}
