2b02df2cb078e2d9c9617575f72ba9608288a82e
RoCl-24 <94844914+RoCl-24@users.noreply.github.com>
2022-03-15 17:50:05 +0100

    Update constantblackscholesprocess.hpp

--- project1/constantblackscholesprocess.hpp
+++ project1/constantblackscholesprocess.hpp
@@ -5,9 +5,27 @@ namespace QuantLib {
 
     class ConstantBlackScholesProcess : public StochasticProcess1D {
 
-        // your implementation goes here
+    private:
+        const double spot;
+        const double rf_rate;
+        const double dividend;
+        const double volatility;
+        
+    
+
+    public:
+        ConstantBlackScholesProcess();
+        ConstantBlackScholesProcess(const ConstantBlackScholesProcess & process);
+        ConstantBlackScholesProcess(double spot, double rf_rate, double dividend, double volatility);
+        ~ConstantBlackScholesProcess();
+
+        double getSpot();
+        double getRf_rate();
+        double getDividend();
+        double getVolatility();
+
         
     };
 
-}
 
+}
