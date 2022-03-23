1cfdb7fa4f6ab3cd48593d532d0458058b26ee15
Yassine-Kh <yassine.khalsi98@gmail.com>
2022-03-23 03:20:05 +0100

    Fixinf issue with constantblackscholesprocess class

--- project1/constantblackscholesprocess.cpp
+++ project1/constantblackscholesprocess.cpp
@@ -1,20 +1,22 @@
 #include "constantblackscholesprocess.hpp"
+#include <ql/processes/eulerdiscretization.hpp>
 
-constantblackscholesprocess::constantblackscholesprocess() {
+constantblackscholesprocess::constantblackscholesprocess() :StochasticProcess1D(ext::make_shared<EulerDiscretization>()) {
     spot = 100.;
     rf_rate = 0.05;
     dividend = 3.;
     volatility = 1.;
 }
 
-constantblackscholesprocess::constantblackscholesprocess(constantblackscholesprocess& process_ref) {
+constantblackscholesprocess::constantblackscholesprocess(constantblackscholesprocess &process_ref) :StochasticProcess1D(ext::make_shared<EulerDiscretization>()){
     spot = process_ref.getSpot();
     rf_rate = process_ref.getRf_rate();
     dividend = process_ref.getDividend();
     volatility = process_ref.getVolatility();
 }
 
-constantblackscholesprocess::constantblackscholesprocess(double spot1, double rf_rate1, double dividend1, double volatility1) {
+constantblackscholesprocess::constantblackscholesprocess(double spot1, double rf_rate1, double dividend1,
+                                                         double volatility1)  :StochasticProcess1D(ext::make_shared<EulerDiscretization>()){
     spot = spot1;
     rf_rate = rf_rate1;
     dividend = dividend1;
@@ -37,7 +39,7 @@ double constantblackscholesprocess::getDividend() const {
     return dividend;
 }
 
-double constantblackscholesprocess::getVolatility() const{
+double constantblackscholesprocess::getVolatility() const {
     return volatility;
 }
 
@@ -46,14 +48,14 @@ Real constantblackscholesprocess::x0() const {
 }
 
 Real constantblackscholesprocess::drift(Time t, Real x) const {
-    return this->getRf_rate() - this->getDividend() -0.5*getVolatility()*getVolatility();
+    return this->getRf_rate() - this->getDividend() - 0.5 * getVolatility() * getVolatility();
 }
 
 Real constantblackscholesprocess::diffusion(Time t, Real x) const {
     return this->getVolatility();
 }
 
-Real constantblackscholesprocess::apply(Real x0, Real dx) const{
+Real constantblackscholesprocess::apply(Real x0, Real dx) const {
     return x0 * std::exp(dx);
 }
 
